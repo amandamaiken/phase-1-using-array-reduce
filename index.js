881ff3b54823c5698ee47d53fedda58a594ad5aa
Amanda <amandamaiken@berkeley.edu>
2022-01-14 10:32:31 -0800

    commit tree c0509d28b5fa0a02e38a43fd97221179ef9d489f

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(results, batteries) {
+    return results + batteries;
+})
 // Code your solution here
